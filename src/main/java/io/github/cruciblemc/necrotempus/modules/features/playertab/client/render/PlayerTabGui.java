4cd10ef76dca814a3b4416bde27236d43183dd4c
w33d <earlysun.984190929@gmail.com>
2025-09-03 07:50:31 +0800

    final

--- src/main/java/io/github/cruciblemc/necrotempus/modules/features/playertab/client/render/PlayerTabGui.java
+++ src/main/java/io/github/cruciblemc/necrotempus/modules/features/playertab/client/render/PlayerTabGui.java
@@ -356,14 +356,18 @@ public class PlayerTabGui extends Gui {
                                 )
                         )
         );
- 
-        minecraft.getTextureManager().bindTexture(icons);
 
-        zLevel += 100.0F;
-        GL11.glPushMatrix();
-        drawTexturedModalRect(minX + maxCellSize - 11, minY, 0, 176 + (pingStatusIcon * 8), 10, 8);
-        GL11.glPopMatrix();
-        zLevel -= 100.0F;
+
+        if (!NecroTempusConfig.drawNumberedPing) {
+            minecraft.getTextureManager().bindTexture(icons);
+
+            zLevel += 100.0F;
+            GL11.glPushMatrix();
+            drawTexturedModalRect(minX + maxCellSize - 11, minY, 0, 176 + (pingStatusIcon * 8), 10, 8);
+            GL11.glPopMatrix();
+            zLevel -= 100.0F;
+            return;
+        }
 
         int[] color = new int[]{-16711936, -256, -14336, -65536, -8355712, -1};
         String ping = tabCell.getPlayerPing() + "ms";
